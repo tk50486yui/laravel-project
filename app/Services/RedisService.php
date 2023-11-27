8801ef949aa5b11861d4e46119bf01553c2e4e92
Yue <tk50486yui@gmail.com>
2023-11-27 18:49:56 +0800

    feat: redis ping exception

--- app/Services/RedisService.php
+++ app/Services/RedisService.php
@@ -6,43 +6,59 @@ use Illuminate\Support\Facades\Redis;
 class RedisService
 {
     public function cache($prefix, $funName, $callback, $expiration = 600)
-    {
-        $redisKey = $prefix . ':' . $funName;
-        $cacheResult = Redis::get($redisKey);
+    {   
+        try {
+            $response = Redis::ping();
+            if($response == 'PONG'){
+                $redisKey = $prefix . ':' . $funName;
+                $cacheResult = Redis::get($redisKey);
 
-        if ($cacheResult) {
-            return json_decode($cacheResult, true);
+                if ($cacheResult) {
+                    return json_decode($cacheResult, true);
+                }
+
+                $result = $callback();
+
+                Redis::setex($redisKey, $expiration, json_encode($result));
+
+                return $result;
+            }
+          
+            return $callback();
+        }catch (\Predis\Connection\ConnectionException $e) { 
+            return $callback();
         }
-
-        $result = $callback();
-
-        Redis::setex($redisKey, $expiration, json_encode($result));
-
-        return $result;
     }
 
     public function update($prefix, $Service, $expiration = 600)
-    {        
-        switch ($prefix) {
-            case 'Words':
-            case 'Articles': 
-            case 'TagsColor': 
-            case 'WordsGroups':
-                $result = $Service->findAll();
-                $redisKey = $prefix. ':findAll';
-                Redis::setex($redisKey, $expiration, json_encode($result));
-                break;
-            case 'Categories':
-            case 'Tags':
-                $result = $Service->findAll();
-                $redisKey = $prefix. ':findAll';
-                Redis::setex($redisKey, $expiration, json_encode($result));
-                $result = $Service->findRecent();
-                $redisKey = $prefix. ':findRecent';
-                Redis::setex($redisKey, $expiration, json_encode($result));
-                break;
-            default:
-                break;
+    {
+        try {
+            $response = Redis::ping();
+            if($response == 'PONG'){
+                switch ($prefix) {
+                    case 'Words':
+                    case 'Articles':
+                    case 'TagsColor':
+                    case 'WordsGroups';
+                        $result = $Service->findAll();
+                        $redisKey = $prefix. ':findAll';
+                        Redis::setex($redisKey, $expiration, json_encode($result));
+                        break;
+                    case 'Categories':
+                    case 'Tags':
+                        $result = $Service->findAll();
+                        $redisKey = $prefix. ':findAll';
+                        Redis::setex($redisKey, $expiration, json_encode($result));
+                        $result = $Service->findRecent();
+                        $redisKey = $prefix. ':findRecent';
+                        Redis::setex($redisKey, $expiration, json_encode($result));
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }catch (\Predis\Connection\ConnectionException $e) {
+            return;
         }
     }
 }
